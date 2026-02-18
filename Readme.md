e47b708aecd24f786c619e7136a88d4d180df60c
Spidey78 <156900471+Spidey78@users.noreply.github.com>
2026-02-18 20:12:48 +0530

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,10 @@
+exports['SD_textui']:ShowTextUI({
+    position = 'bottom', ------replace position Right, Left, Bottom.
+    key = 'E', 
+    title = 'title',
+    description = 'text',
+    color = '#22c55e'
+})
+
+-- Hide
+exports['SD_textui']:HideTextUI()
---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'SD Scripts'
+description 'SD TextUI'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
+
+client_scripts {
+    'client/client.lua'
+}
+
+-- 🔥 REQUIRED
+exports {
+    'ShowTextUI',
+    'HideTextUI',
+    'IsTextUIOpen'
+}
